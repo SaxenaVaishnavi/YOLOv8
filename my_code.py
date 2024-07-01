c551e5c18bf2e8762127a793b848536945c733f6
Vaishnavi Saxena <128722267+SaxenaVaishnavi@users.noreply.github.com>
2024-07-01 12:21:47 +0530

    my_code.py

--- my_code.py
+++ my_code.py
@@ -1,3 +1,11 @@
 from ultralytics import YOLO
+
+# Loading the YOLOv8 (Version 8) Model
 model = YOLO('yolov8n.pt')
-results = model(source='test4.mp4', show=True, conf=0.4, save=True)
+
+# Running inference on the source
+'''in the source argument:
+- give the path of the test file that you want to run the model on, if the test file is in a different folder
+- give the name of the file if the the file is in the present owrking directory only
+- give source = 0 if using the default webcam for live streaming'''
+results = model(source='name_of_test_file', show=True, conf=0.4, save=True)
